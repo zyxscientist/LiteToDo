6e691057b38405c496343df4f865d6251bfcb8dc
PeterZhu <zyxchelsea11@gmail.com>
2022-09-06 22:10:58 +0800

    Initial Commit

---
+++ LiteToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiteToDo
+//
+//  Created by 朱宇軒 on 2022/9/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiteToDo/LiteToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiteToDoApp.swift
+//  LiteToDo
+//
+//  Created by 朱宇軒 on 2022/9/6.
+//
+
+import SwiftUI
+
+@main
+struct LiteToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
